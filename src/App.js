0170d36d6a374cd916a128890a1b3d47d08e796a
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-10-31 10:17:22 +0100

    :spakles: set the size of the button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "./App.css";
 const ButtonStyled = styled.button`
   display: inline-block;
   color: #111;
-  font-size: 5em;
+  font-size: 1em;
   margin: 1em;
+  width: 5em;
   padding: 0.25em 0.5em;
   border: 2px solid #30eccdbb;
   border-radius: 5px;
